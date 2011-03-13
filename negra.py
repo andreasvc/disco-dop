08cf3bd06ceeb3184b9165e2d14c2d23f9db26cd
Andreas van Cranenburgh <andreas@unstable.nl>
2011-03-13 18:13:23 +0100

    Negra corpus reader

---
+++ negra.py
@@ -0,0 +1,35 @@
+from nltk.corpus.reader.api import CorpusReader, SyntaxCorpusReader
+from nltk.corpus.reader.util import read_regexp_block
+from nltk import Tree
+import re
+
+BOS = re.compile("^#BOS")
+EOS = re.compile("^#EOS")
+WORD, LEMMA, TAG, MORPH, FUNC, PARENT = range(6)
+
+class NegraCorpusReader(SyntaxCorpusReader):
+	def __init__(self, root, fileids, encoding=None):
+		CorpusReader.__init__(self, root, fileids, encoding)
+	def _parse(self, s):
+		def getchildren(parent):
+			for n,a in ((n,a) for n,a in enumerate(s) if a[PARENT] == parent):
+				if a[WORD][0] == "#":
+					yield Tree(a[TAG], getchildren(a[WORD][1:]))
+				else:
+					#yield Tree(a[TAG], [a[WORD]])
+					yield Tree(a[TAG], [n])
+		return Tree("ROOT", getchildren("0"))
+	def _word(self, s):
+		return [a[WORD] for a in s if a[WORD][0] != "#"]
+	def _tag(self, s, ignore):
+		return [(a[WORD], a[TAG]) for a in s if a[WORD][0] != "#"]
+	def _read_block(self, stream):
+		return [[line.split() for line in block.splitlines()[1:]] for block in read_regexp_block(stream, BOS, EOS)]
+
+def main():
+	n = NegraCorpusReader(".", "sample2\.export")
+	print n.parsed_sents()
+	print n.tagged_sents()
+	print n.sents()
+
+if __name__ == '__main__': main()
