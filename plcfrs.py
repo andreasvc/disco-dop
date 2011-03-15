7a6f78084f3beb63562049d482a44b7b5f4b80ad
Andreas van Cranenburgh <andreas@unstable.nl>
2011-03-15 03:15:19 +0100

    some optimizations

--- plcfrs.py
+++ plcfrs.py
@@ -1,6 +1,6 @@
 # probabilistic CKY parser for Simple Range Concatenation Grammars
 # (equivalent to Linear Context-Free Rewriting Systems)
-from nltk import FeatStruct, FeatList, featstruct, FreqDist, Tree
+from nltk import FeatStruct, FeatList, featstruct, FreqDist, Tree, ImmutableTree
 from math import log, e
 from random import choice
 from itertools import chain, product, islice
@@ -35,12 +35,11 @@ def parse(sent, grammar, start="S", viterbi=False):
 					word = fs("[[%s, [%d]], %d]" % (repr(rule[0][0]), i, i))
 					yield word, z
 					#yield rule, z
-	def deduced_from((I, x), C):
+	def deduced_from(I, x, C):
 		for rule, z in unary[I[0]]:
 			r = concat(rule.unify(FeatList([lhs, I])))
 			if all(map(contiguous, r[0][1:])): yield r, z
-		for Ic1, y in C.items():
-			I1 = Ic1[0]
+		for I1 in C:
 			#detect overlap in ranges
 			if any(a & b for a,b in product(map(set, I1[1:]), map(set, I[1:]))): continue
 			for rule, z in binary[(I[0], I1[0])]:
@@ -50,21 +49,24 @@ def parse(sent, grammar, start="S", viterbi=False):
 				right = concat(rule.unify(FeatList([lhs, I1, I])))
 				if all(map(contiguous, right[0][1:])): yield right, z
 			
-	A, C = {}, {}
+	A, C, Cx = {}, defaultdict(list), defaultdict(list)
 	A.update(scan(sent))
 	while A:	
 		I, x = max(A.items(), key=lambda x: x[1])
 		del A[I]
-		C[I] = x
+		if I not in C[I[0]]:
+			C[I[0]].append(I)
+			Cx[I[0]].append(x)
 		if I[0] == goal:
-			if viterbi: return C, goal
+			if viterbi: break
 		else:
-			for I1, y in deduced_from((I[0], x), C):
-				if I1 not in C and I1 not in A:
+			for I1, y in deduced_from(I[0], x, C.keys()):
+				if I1 not in C[I1[0]] and I1 not in A:
 					A[I1] = y
 				elif I1 in A:
 					A[I1] = max(y, A[I1])
-	if any(I[0] == goal for I in C): return C, goal
+	for a in C.keys(): C[a] = [(b[1:],p) for b,p in zip(C[a], Cx[a])]
+	if goal in C: return C, goal
 	else: return {}, []
 
 def cartpi(seq):
@@ -73,36 +75,28 @@ def cartpi(seq):
 	return ((), )
 
 def enumchart(chart, start):
-	"""enumerate trees in chart headed by start in top down fashion. chart is
-		a dictionary of FeatStructs and logprobs"""
-	def enumchart1(chart, start):
-		for a,p in chart[start]:
-			if len(a) == 1 and isinstance(a[0], int):
-				yield Tree(start[0], a), p
-				continue
-			for x in cartpi(map(lambda y: enumchart1(chart, y), a)):
-				yield Tree(start[0], [z[0] for z in x]), p+x[0][1]+x[1][1]
-	chart1 = defaultdict(list)
-	for a,p in chart.items(): chart1[a[0]].append((a[1:],p))
-	return enumchart1(chart1, start)
+	"""exhaustively enumerate trees in chart headed by start in top down fashion. 
+		chart is a dictionary with lhs -> (rhs, logprob) """
+	for a,p in chart[start]:
+		if len(a) == 1 and isinstance(a[0], int):
+			yield Tree(start[0], a), p
+			continue
+		for x in cartpi(map(lambda y: enumchart(chart, y), a)):
+			yield Tree(start[0], [z[0] for z in x]), p+x[0][1]+x[1][1]
 
 def samplechart(chart, start):
-	def samplechart1(chart, start):
-		entry, p = choice(chart[start])
-		if len(entry) == 1 and isinstance(entry[0], int):
-			return Tree(str(start[0]), entry), p
-		children = [samplechart1(chart, a) for a in entry]
-		tree = Tree(start[0], [a for a,b in children]) 
-		return tree, p+sum(b for a,b in children)
-	chart1 = defaultdict(list)
-	for a,p in chart.items(): chart1[a[0]].append((a[1:],p))
-	while True: yield samplechart1(chart1, start)
+	entry, p = choice(chart[start])
+	if len(entry) == 1 and isinstance(entry[0], int):
+		return ImmutableTree(str(start[0]), entry), p
+	children = [samplechart(chart, a) for a in entry]
+	tree = ImmutableTree(start[0], [a for a,b in children]) 
+	return tree, p+sum(b for a,b in children)
 	
 def mostprobableparse(chart, start, n=100):
 		""" sum over n random derivations from chart, 
 			return a FreqDist of parse trees, with .max() being the MPP"""
 		l = FreqDist()
-		for a,prob in islice(samplechart(chart, start), n):
+		for a,prob in set(map(lambda x: samplechart(chart, start), range(n))):
 			l.inc(re.sub(r"@[0-9]+", "", a.pprint(margin=999)), e**prob)
 		return l
 
