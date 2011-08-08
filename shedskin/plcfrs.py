6628f148876ed3c443ae9317ebd911da3d12c0ce
andreasvc <andreas@unstable.nl>
2011-08-08 02:12:05 +0200

    improved shedskin version: splitgrammar

--- shedskin/plcfrs.py
+++ shedskin/plcfrs.py
@@ -1,13 +1,13 @@
 from math import exp, log
 from array import array
-from collections import defaultdict
+from itertools import chain
 from heapq import heappush, heappop, heapify
 
 print "plcfrs in shedskin mode"
 
 def parse(sent, grammar, tags, start, exhaustive):
     """ parse sentence, a list of tokens, optionally with gold tags, and
-    produce a chart, either exhaustive (n=0) or up until the viterbi parse
+    produce a chart, either exhaustive or up until the viterbi parse
     """
     unary = grammar.unary
     lbinary = grammar.lbinary
@@ -26,14 +26,14 @@ def parse(sent, grammar, tags, start, exhaustive):
     Epsilon = toid["Epsilon"]
     for i, w in enumerate(sent):
         recognized = False
-	for terminal in lexical.get(w, []):
-	    if not tags or tags[i] == tolabel[terminal.lhs].split("@")[0]:
-		Ih = ChartItem(terminal.lhs, 1 << i)
-		I = ChartItem(Epsilon, i)
-		z = terminal.prob
-		A[Ih] = Edge(z, z, z, I, None)
-		C[Ih] = []
-		recognized = True
+        for terminal in lexical.get(w, []):
+            if not tags or tags[i] == tolabel[terminal.lhs].split("@")[0]:
+                Ih = ChartItem(terminal.lhs, 1 << i)
+                I = ChartItem(Epsilon, i)
+                z = terminal.prob
+                A[Ih] = Edge(z, z, z, I, None)
+                C[Ih] = []
+                recognized = True
         if not recognized and tags and tags[i] in toid:
             Ih = ChartItem(toid[tags[i]], 1 << i)
             I = ChartItem(Epsilon, i)
@@ -73,7 +73,6 @@ def parse(sent, grammar, tags, start, exhaustive):
     print "max agenda size", maxA, "/ chart keys", len(C),
     print "/ values", sum(map(len, C.values()))
     if goal not in C:
-        C = {}
         goal = None
     return (C, goal)
 
@@ -101,7 +100,7 @@ def concat(rule, lvec, rvec):
     if lvec & rvec: return False
     lpos = nextset(lvec, 0)
     rpos = nextset(rvec, 0)
-    #this algorithm taken from rparse FastYFComposer.
+    #this algorithm was taken from rparse, FastYFComposer.
     for x in range(len(rule.args)):
         m = rule.lengths[x] - 1
         for n in range(m + 1):
@@ -124,7 +123,7 @@ def concat(rule, lvec, rvec):
                     return False
                 #jump to next argument
                 rpos = nextset(rvec, rpos)
-            else: #if bit == 0:
+            else:
                 # vice versa to the above
                 if lpos == -1 or (rpos != -1 and rpos <= lpos):
                     return False
@@ -166,7 +165,7 @@ def binrepr(a, sent):
 def pprint_chart(chart, sent, tolabel):
     print "chart:"
     for n, a in sorted((bitcount(a.vec), a) for a in chart):
-	if not chart[a]: continue
+        if not chart[a]: continue
         print "%s[%s] =>" % (tolabel[a.label], binrepr(a, sent))
         for edge in chart[a]:
             print "%g\t%g" % (exp(-edge.inside), exp(-edge.prob)),
@@ -185,16 +184,12 @@ def do(sent, grammar):
     print "sentence", sent
     chart, start = parse(sent.split(), grammar, None, -1, False)
     pprint_chart(chart, sent.split(), grammar.tolabel)
-    if chart:
-        #for t, p in mostprobableparse(chart, start, grammar.tolabel,
-        #                                                   n=1000).items():
-        #   print p, t
+    if start:
         t, p = mostprobablederivation(chart, start, grammar.tolabel)
         print exp(-p), t, '\n'
-        return True
     else:
         print "no parse"
-        return False
+    return start is not None
 
 # bit operations
 def nextset(a, pos):
@@ -223,6 +218,7 @@ def testbit(a, offset):
     """ Mask a particular bit, return nonzero if set """
     return a & (1 << offset)
 
+# various data types
 class Grammar(object):
     __slots__ = ('unary', 'lbinary', 'rbinary', 'lexical',
                     'bylhs', 'toid', 'tolabel')
@@ -246,17 +242,12 @@ class ChartItem:
     def __eq__(self, other):
         if other is None: return False
         return self.label == other.label and self.vec == other.vec
-    def __repr__(self):
-        return "ChartItem(%s, %s)" % (self.label, bin(self.vec))
 
 class Edge:
-    __slots__ = ('score', 'inside', 'prob', 'left', 'right', '_hash')
+    __slots__ = ('score', 'inside', 'prob', 'left', 'right')
     def __init__(self, score, inside, prob, left, right):
         self.score = score; self.inside = inside; self.prob = prob
         self.left = left; self.right = right
-        self._hash = hash(((inside, prob), (left, right)))
-    def __hash__(self):
-        return self._hash
     def __lt__(self, other):
         # the ordering only depends on inside probability
         # (or on estimate of outside score when added)
@@ -274,11 +265,6 @@ class Edge:
         return self.score > other.score
     def __ge__(self, other):
         return self.score >= other.score
-    def __repr__(self):
-        return "Edge(%g, %g, %g, %r, %r)" % (self.score, self.inside,
-                    self.prob,
-                    self.left,
-                    self.right)
 
 class Terminal:
     __slots__ = ('lhs', 'rhs1', 'rhs2', 'word', 'prob')
@@ -293,6 +279,67 @@ class Rule:
         self.lhs = lhs; self.rhs1 = rhs1; self.rhs2 = rhs2
         self.args = args; self.lengths = lengths; self.prob = prob
         self._args = self.args; self._lengths = self.lengths
+
+def splitgrammar(grammar, lexicon):
+    """ split the grammar into various lookup tables, mapping nonterminal
+    labels to numeric identifiers. Also negates log-probabilities to
+    accommodate min-heaps.
+    Can only represent ordered SRCG rules (monotone LCFRS). """
+    # get a list of all nonterminals; make sure Epsilon and ROOT are first, and
+    # assign them unique IDs
+    nonterminals = list(enumerate(["Epsilon", "ROOT"]
+        + sorted(set(nt for (rule, yf), weight in grammar for nt in rule)
+            - set(["Epsilon", "ROOT"]))))
+    toid = dict((lhs, n) for n, lhs in nonterminals)
+    tolabel = dict((n, lhs) for n, lhs in nonterminals)
+    bylhs = [[] for _ in nonterminals]
+    unary = [[] for _ in nonterminals]
+    lbinary = [[] for _ in nonterminals]
+    rbinary = [[] for _ in nonterminals]
+    lexical = {}
+    arity = array('B', [0] * len(nonterminals))
+    for (tag, word), w in lexicon:
+        t = Terminal(toid[tag[0]], toid[tag[1]], 0, word[0], abs(w))
+        assert arity[t.lhs] in (0, 1)
+        arity[t.lhs] = 1
+        lexical.setdefault(word[0], []).append(t)
+    for (rule, yf), w in grammar:
+        args, lengths = yfarray(yf)
+        assert yf == arraytoyf(args, lengths) # unbinarized rule => error
+        #cyclic unary productions
+        if len(rule) == 2 and w == 0.0: w += 0.00000001
+        r = Rule(toid[rule[0]], toid[rule[1]],
+            toid[rule[2]] if len(rule) == 3 else 0, args, lengths, abs(w))
+        if arity[r.lhs] == 0:
+            arity[r.lhs] = len(args)
+        assert arity[r.lhs] == len(args)
+        if len(rule) == 2:
+            unary[r.rhs1].append(r)
+            bylhs[r.lhs].append(r)
+        elif len(rule) == 3:
+            lbinary[r.rhs1].append(r)
+            rbinary[r.rhs2].append(r)
+            bylhs[r.lhs].append(r)
+        else: raise ValueError("grammar not binarized: %r" % r)
+    #assert 0 not in arity[1:]
+    return Grammar(unary, lbinary, rbinary, lexical, bylhs, toid, tolabel)
+
+def yfarray(yf):
+    """ convert a yield function represented as a 2D sequence to an array
+    object. """
+    # I for 32 bits (int), H for 16 bits (short), B for 8 bits (char)
+    vectype = 'I'; vecsize = 32 #8 * array(vectype).itemsize
+    lentype = 'H'; lensize = 16 #8 * array(lentype).itemsize
+    assert len(yf) <= lensize
+    assert all(len(a) <= vecsize for a in yf)
+    initializer = [sum(1 << n for n, b in enumerate(a) if b) for a in yf]
+    args = array('I', initializer)
+    lengths = array('H', map(len, yf))
+    return args, lengths
+
+def arraytoyf(args, lengths):
+    return tuple(tuple(1 if a & (1 << m) else 0 for m in range(n))
+                            for n, a in zip(lengths, args))
 
 #the agenda (priority queue)
 class Entry(object):
@@ -309,21 +356,13 @@ class Entry(object):
     def __le__(self, other):
         return self.value < other.value or (self.value == other.value
                 and self.count <= other.count)
-    def __hash__(self):
-        return hash(((self.key, self.value), self.count))
 
 INVALID = 0
 class agenda(object):
-    def __init__(self, iterable=None):
+    def __init__(self):
         self.heap = []                      # the priority queue list
         self.counter = 1                    # unique sequence count
         self.mapping = {}                   # mapping of keys to entries
-    #   if iterable:
-    #       self.heap = [Entry(k, v, n + 1)
-    #                       for n, (k,v) in enumerate(dict(iterable).items())]
-    #       heapify(self.heap)
-    #       self.mapping = dict((entry.key, entry) for entry in self.heap)
-    #       self.counter += len(self.heap)
 
     def __setitem__(self, key, value):
         if key in self.mapping:
@@ -342,9 +381,6 @@ class agenda(object):
     def __getitem__(self, key):
         return self.mapping[key].value
 
-    def __delitem__(self, key):
-        self.mapping.pop(key).count = INVALID
-
     def __contains__(self, key):
         return key in self.mapping
 
@@ -362,53 +398,31 @@ class agenda(object):
         return entry.key, entry.value
 
 def main():
-    '''
-    grammar = splitgrammar(
-        [((('S','VP2','VMFIN'),    ((0,1,0),)),  0.0),
+    rules = [
+        ((('S','VP2','VMFIN'),    ((0,1,0),)),   log(1.0)),
         ((('VP2','VP2','VAINF'),  ((0,),(0,1))), log(0.5)),
-        ((('VP2','PROAV','VVPP'), ((0,),(1,))), log(0.5)),
-        ((('PROAV', 'Epsilon'), ('Daruber', ())), 0.0),
-        ((('VAINF', 'Epsilon'), ('werden', ())), 0.0),
-        ((('VMFIN', 'Epsilon'), ('muss', ())), 0.0),
-        ((('VVPP', 'Epsilon'), ('nachgedacht', ())), 0.0)])
-    '''
-    # output of splitgrammar:
-    unary=[[], [], [], [], [], [],
-        [Rule(6, 6, 0, array('I', [3L]), array('H', [2]), -log(0.1))], []]
-    lbinary=[[], [], [Rule(6, 2, 7, array('I', [0L, 1L]), array('H', [1, 1]),
-        -log(0.5))], [], [], [], [Rule(3, 6, 5, array('I', [2L]), array('H',
-        [3]), 0.0), Rule(6, 6, 4, array('I', [0L, 2L]), array('H', [1, 2]),
-        -log(0.5))], []]
-    rbinary=[[], [], [], [], [Rule(6, 6, 4, array('I', [0L, 2L]), array('H',
-        [1, 2]), -log(0.5))], [Rule(3, 6, 5, array('I', [2L]), array('H', [3]),
-        0.0)], [], [Rule(6, 2, 7, array('I', [0L, 1L]), array('H', [1, 1]),
-        -log(0.5))]]
-    lexical={'muss': [Terminal(5, 0, 0, 'muss', 0.0)],
-            'werden': [Terminal(4, 0, 0, 'werden', 0.0)],
-            'Daruber': [Terminal(2, 0, 0, 'Daruber', 0.0)],
-            'nachgedacht': [Terminal(7, 0, 0, 'nachgedacht', 0.0)]}
-    bylhs=[[], [], [], [Rule(3, 6, 5, array('I', [2L]), array('H', [3]), 0.0)],
-        [], [], [Rule(6, 6, 4, array('I', [0L, 2L]), array('H', [1, 2]),
-        -log(0.5)), Rule(6, 2, 7, array('I', [0L, 1L]), array('H', [1,
-        1]), -log(0.5))], []] 
-    toid = {'VP2': 6, 'Epsilon': 0, 'VVPP': 7, 'S': 3, 'VMFIN': 5, 'VAINF': 4,
-        'ROOT': 1, 'PROAV': 2 }
-    tolabel = {0: 'Epsilon', 1: 'ROOT', 2: 'PROAV', 3: 'S', 4: 'VAINF', 5:
-        'VMFIN', 6: 'VP2', 7: 'VVPP'}
-    grammar = Grammar(unary, lbinary, rbinary, lexical, bylhs, toid, tolabel)
+        ((('VP2','PROAV','VVPP'), ((0,),(1,))),  log(0.5)),
+        ((('VP2','VP2'),          ((0,),(0,))),  log(0.1))]
+    lexicon = [
+        ((('PROAV', 'Epsilon'), ('Daruber', ())),     0.0),
+        ((('VAINF', 'Epsilon'), ('werden', ())),      0.0),
+        ((('VMFIN', 'Epsilon'), ('muss', ())),        0.0),
+        ((('VVPP', 'Epsilon'),  ('nachgedacht', ())), 0.0)]
+    grammar = splitgrammar(rules, lexicon)
 
     chart, start = parse("Daruber muss nachgedacht werden".split(),
-            grammar, "PROAV VMFIN VVPP VAINF".split(), toid['S'], False)
-    pprint_chart(chart, "Daruber muss nachgedacht werden".split(), tolabel)
-    assert (mostprobablederivation(chart, start, tolabel) ==
+          grammar, "PROAV VMFIN VVPP VAINF".split(), grammar.toid['S'], False)
+    pprint_chart(chart, "Daruber muss nachgedacht werden".split(),
+          grammar.tolabel)
+    assert (mostprobablederivation(chart, start, grammar.tolabel) ==
         ('(S (VP2 (VP2 (PROAV 0) (VVPP 2)) (VAINF 3)) (VMFIN 1))', -log(0.25)))
     assert do("Daruber muss nachgedacht werden", grammar)
     assert do("Daruber muss nachgedacht werden werden", grammar)
     assert do("Daruber muss nachgedacht werden werden werden", grammar)
     print "ungrammatical sentence:"
-    assert not do("muss Daruber nachgedacht werden", grammar)
+    assert not do("werden nachgedacht muss Daruber", grammar)
     print "(as expected)\n"
     print 'it worked'
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
