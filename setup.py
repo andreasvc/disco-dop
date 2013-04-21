a2dece705afc347ecece15e8bbebd6aa4176f5f6
Andreas van Cranenburgh <andreas@unstable.nl>
2013-04-21 23:29:35 +0200

    simplify setup.py

--- setup.py
+++ setup.py
@@ -7,32 +7,26 @@ import numpy
 
 # some of these directives increase performance,
 # but at the cost of failing in mysterious ways.
-cython_directives = dict(
-	profile=False,
-	cdivision=True,
-	nonecheck=False,
-	wraparound=False,
-	boundscheck=False,
-	embedsignature=True,
-	language_level=3
+directives = dict(
+		profile=False,
+		cdivision=True,
+		nonecheck=False,
+		wraparound=False,
+		boundscheck=False,
+		embedsignature=True,
 )
 
 #Options.fast_fail = True
 #Options.extra_compile_args = ["-O3"]
 #Options.extra_link_args = ["-O3"]  #["-g"],
 if __name__ == '__main__':
-	setup(
-		name = 'disco-dop',
-		include_dirs = [numpy.get_include()], #cythonutils],
-		cmdclass = {'build_ext': build_ext},
-		ext_modules = cythonize(
-			'*.pyx',
-			#[Extension('*', ['*.pyx'],
-			#	extra_compile_args=["-O3"],
-			#	extra_link_args=["-O3"], #["-g"],
-			#)],
-			nthreads=4,
-			annotate=True,
-			cython_directives=cython_directives,
+	setup(name = 'disco-dop',
+			include_dirs = [numpy.get_include()],
+			cmdclass = dict(build_ext=build_ext),
+			ext_modules = cythonize('*.pyx',
+					nthreads=4,
+					annotate=True,
+					#language_level=3, # FIXME make this work ...
+					compiler_directives=directives,
 			)
 	)
