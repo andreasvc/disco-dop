5806db9ac1d11e78f60b4a599bb0a5e4e4496e84
Andreas van Cranenburgh <andreas@unstable.nl>
2013-05-15 14:42:03 +0200

    increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Cython.Distutils import build_ext
 import numpy
 
 metadata = dict(name='disco-dop',
-		version='0.1',
+		version='0.2',
 		description='Discontinuous Data-Oriented Parsing',
 		long_description=open('README.rst').read(),
 		author='Andreas van Cranenburgh',
