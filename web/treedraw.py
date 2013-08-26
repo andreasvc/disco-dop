fcb4abf11722d38fa9ef32e76bda7e4e32dc7d2d
Andreas van Cranenburgh <andreas@unstable.nl>
2013-08-26 17:14:29 +0200

    generate latex code only when necessary

--- web/treedraw.py
+++ web/treedraw.py
@@ -68,7 +68,38 @@ def draw():
 
 def drawtrees(form, dts):
 	""" Draw trees in the requested format. """
-	os.chdir('/tmp')
+	if form.get('output', 'text') == 'svg':
+		if len(dts) == 1:
+			return Response(dts[0].svg().encode('utf-8'),
+					mimetype='image/svg+xml')
+		else:
+			result = [('<!doctype html>\n<html>\n<head>\n'
+				'\t<meta http-equiv="Content-Type" '
+				'content="text/html; charset=UTF-8">\n</head>\n<body>')]
+			for dt in dts:
+				result.append(
+						'<div>\n%s\n</div>\n\n' % dt.svg().encode('utf-8'))
+			result.append('</body></html>')
+			return Response('\n'.join(result), mimetype='text/html')
+	elif form.get('output', 'text') == 'text':
+		html = form.get('color', False)
+		ascii = not form.get('unicode', 0)
+		result = []
+		if html:
+			mimetype = 'text/html'
+			result.append('<!doctype html>\n<html>\n<head>\n'
+				'\t<meta http-equiv="Content-Type" '
+				'content="text/html; charset=UTF-8">\n</head>\n<body>\n<pre>')
+		else:
+			mimetype = 'text/plain'
+		for dt in dts:
+			result.append(
+					dt.text(unicodelines=not ascii, html=html).encode('utf-8'))
+		if html:
+			result.append('</pre></body></html>')
+		return Response('\n'.join(result), mimetype=mimetype)
+
+	# LaTeX based output
 	if form.get('type', 'matrix') == 'matrix':
 		latexcode = ''.join((PREAMBLE % (
 			'\\usepackage{helvet,tikz}\n'
@@ -102,71 +133,41 @@ def drawtrees(form, dts):
 				POSTAMBLE))
 	if form.get('output', 'text') == 'latex':
 		return Response(latexcode, mimetype='text/plain')
-	elif form.get('output', 'text') == 'svg':
-		if len(dts) == 1:
-			return Response(dts[0].svg().encode('utf-8'),
-					mimetype='image/svg+xml')
-		else:
-			result = [('<!doctype html>\n<html>\n<head>\n'
-				'\t<meta http-equiv="Content-Type" '
-				'content="text/html; charset=UTF-8">\n</head>\n<body>')]
-			for dt in dts:
-				result.append(
-						'<div>\n%s\n</div>\n\n' % dt.svg().encode('utf-8'))
-			result.append('</body></html>')
-			return Response('\n'.join(result), mimetype='text/html')
-	elif form.get('output', 'text') == 'text':
-		html = form.get('color', False)
-		ascii = not form.get('unicode', 0)
-		result = []
-		if html:
-			mimetype = 'text/html'
-			result.append('<!doctype html>\n<html>\n<head>\n'
-				'\t<meta http-equiv="Content-Type" '
-				'content="text/html; charset=UTF-8">\n</head>\n<body>\n<pre>')
-		else:
-			mimetype = 'text/plain'
-		for dt in dts:
-			result.append(
-					dt.text(unicodelines=not ascii, html=html).encode('utf-8'))
-		if html:
-			result.append('</pre></body></html>')
-		return Response('\n'.join(result), mimetype=mimetype)
-	else:
-		with open('/tmp/dtree.tex', 'w') as tex:
-			tex.write(latexcode)
-		proc = Popen('/usr/bin/pdflatex -halt-on-error  /tmp/dtree.tex'.split(),
+	with open('/tmp/dtree.tex', 'w') as tex:
+		tex.write(latexcode)
+	os.chdir('/tmp')
+	proc = Popen('/usr/bin/pdflatex -halt-on-error  /tmp/dtree.tex'.split(),
+			stdin=None, stdout=PIPE, stderr=PIPE, shell=False)
+	proc.wait()  # pylint: disable=E1101
+	if form.get('output', 'text') == 'pdf':
+		try:
+			return send_from_directory('/tmp', 'dtree.pdf',
+					mimetype='application/pdf')
+		except IOError:
+			pass
+		for ext in ('aux', 'log', 'pdf'):
+			os.remove('/tmp/dtree.' + ext)
+	if form.get('output', 'text') == 'png':
+		proc = Popen('/usr/bin/convert -density 125 /tmp/dtree.pdf \
+				/tmp/dtree.png'.split(),  # -trim
 				stdin=None, stdout=PIPE, stderr=PIPE, shell=False)
 		proc.wait()  # pylint: disable=E1101
-		if form.get('output', 'text') == 'pdf':
+		#set Expires one day ahead (according to server time)
+		#req.headers_out['Expires'] = (
+		#	datetime.utcnow() + timedelta( 1, 0 )
+		#	).strftime('%a, %d %b %Y %H:%M:%S UTC')
+		#req.headers_out['Cache-Control'] = 'max-age=604800, public'
+		try:
+			return send_from_directory('/tmp', 'dtree.png',
+					mimetype='image/png')
+		except IOError:
+			pass
+		for filename in ('/tmp/dtree.aux', '/tmp/dtree.log',
+				'/tmp/dtree.tex', '/tmp/dtree.pdf', '/tmp/dtree.png'):
 			try:
-				return send_from_directory('/tmp', 'dtree.pdf',
-						mimetype='application/pdf')
-			except IOError:
-				pass
-			for ext in ('aux', 'log', 'pdf'):
-				os.remove('/tmp/dtree.' + ext)
-		if form.get('output', 'text') == 'png':
-			proc = Popen('/usr/bin/convert -density 125 /tmp/dtree.pdf \
-					/tmp/dtree.png'.split(),  # -trim
-					stdin=None, stdout=PIPE, stderr=PIPE, shell=False)
-			proc.wait()  # pylint: disable=E1101
-			#set Expires one day ahead (according to server time)
-			#req.headers_out['Expires'] = (
-			#	datetime.utcnow() + timedelta( 1, 0 )
-			#	).strftime('%a, %d %b %Y %H:%M:%S UTC')
-			#req.headers_out['Cache-Control'] = 'max-age=604800, public'
-			try:
-				return send_from_directory('/tmp', 'dtree.png',
-						mimetype='image/png')
-			except IOError:
-				pass
-			for filename in ('/tmp/dtree.aux', '/tmp/dtree.log',
-					'/tmp/dtree.tex', '/tmp/dtree.pdf', '/tmp/dtree.png'):
-				try:
-					os.remove(filename)
-				except OSError:
-					break
+				os.remove(filename)
+			except OSError:
+				break
 
 
 if __name__ == '__main__':
