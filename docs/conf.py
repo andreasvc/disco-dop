320bad4187f9b6f95d875244802ab2ee264a5b71
Andreas van Cranenburgh <andreas@unstable.nl>
2014-01-05 23:46:33 +0100

    disable rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -90,14 +90,14 @@ autodoc_member_order = 'bysource'
 
 # -- Options for HTML output --------------------------------------------------
 
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-	import sphinx_rtd_theme
-	html_theme = 'sphinx_rtd_theme'
-	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+## on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#
+#if not on_rtd:  # only import and set the theme if we're building docs locally
+#	import sphinx_rtd_theme
+#	html_theme = 'sphinx_rtd_theme'
+#	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+## otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
