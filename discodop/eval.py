andreasvc/disco-dop

commit b80f9e6058ce54f6c6bb442b8a46d4990e2cc5d3

eval: avoid zero div when gold has no function tags; fixes #49

discodop/eval.py
@@ -185,9 +185,10 @@ class Evaluator(object):
 			if cat is None:
 				print(39 * ' ', end='')
 			else:
-				print('%s  %6.2f  %s  %s  %s' % (
+				print('%s  %s  %s  %s  %s' % (
 					cat.rjust(7),
-					100 * sum(acc.goldbfunc[cat].values()) / len(acc.goldfun),
+					nozerodiv(lambda: 100 * sum(acc.goldbfunc[cat].values())
+							/ len(acc.goldfun)),
 					nozerodiv(lambda: recall(
 							acc.goldbfunc[cat], acc.candbfunc[cat])),
 					nozerodiv(lambda: precision(

commit 432c0cdc6eb5bfd0fc4acbe00eb5c660ef0642e4

eval: fix function tag percentage bug introduced in b80f9e6

discodop/eval.py
@@ -187,7 +187,7 @@ class Evaluator(object):
 			else:
 				print('%s  %s  %s  %s  %s' % (
 					cat.rjust(7),
-					nozerodiv(lambda: 100 * sum(acc.goldbfunc[cat].values())
+					nozerodiv(lambda: sum(acc.goldbfunc[cat].values())
 							/ len(acc.goldfun)),
 					nozerodiv(lambda: recall(
 							acc.goldbfunc[cat], acc.candbfunc[cat])),