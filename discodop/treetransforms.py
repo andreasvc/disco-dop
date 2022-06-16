d2a0eff19be6d7c099f854b1912250be6d98ce5c
minacode <minamoto9@web.de>
2022-06-16 18:04:51 +0200

    fix collapseunary

--- discodop/treetransforms.py
+++ discodop/treetransforms.py
@@ -329,6 +329,9 @@ def collapseunary(tree, collapsepos=False, collapseroot=False, joinchar='+'):
 					and (collapsepos or isinstance(node[0, 0], Tree))):
 				node.label += joinchar + node[0].label
 				node[0:] = [child for child in node[0]]
+                # remove previous parents from the children
+                for i, child in enumerate(node[0]):
+                    node[0]._delparent(child, i)
 				# since we assigned the child's children to the current node,
 				# evaluate the current node again
 				agenda.append(node)
