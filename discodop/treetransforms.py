6750d1a793373a070d85eca340edb41cf8e27ead
minacode <minamoto9@web.de>
2022-06-16 18:17:19 +0200

    add check for ParentedTree

--- discodop/treetransforms.py
+++ discodop/treetransforms.py
@@ -21,7 +21,7 @@ import re
 from operator import attrgetter
 from itertools import islice
 from collections import defaultdict, Counter
-from .tree import Tree, ImmutableTree, isdisc, bitfanout, HEAD
+from .tree import Tree, ParentedTree, ImmutableTree, isdisc, bitfanout, HEAD
 from .util import OrderedSet, PyAgenda
 
 # e.g., 'VP_2*0' group 1: 'VP_2'; group 2: '0'; group 3: ''
@@ -331,7 +331,8 @@ def collapseunary(tree, collapsepos=False, collapseroot=False, joinchar='+'):
 				node[0:] = [child for child in node[0]]
                 # remove previous parents from the children
                 for i, child in enumerate(node[0]):
-                    node[0]._delparent(child, i)
+                    if isinstance(child, ParentedTree):
+                        node[0]._delparent(child, i)
 				# since we assigned the child's children to the current node,
 				# evaluate the current node again
 				agenda.append(node)
